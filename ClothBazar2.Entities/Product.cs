da2970836320f8c84be6d558f6bdd2fb70283698
waqask357 <waqask357@gmail.com>
2021-04-27 17:00:59 +0500

    ConnectionString

---
+++ ClothBazar2.Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar2.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Catogery Catogery { get; set; }
+    }
+}
---
+++ ClothBazar2.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazar2.Web.Startup))]
+namespace ClothBazar2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
